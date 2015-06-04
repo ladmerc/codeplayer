9a9f94e6172aca6426291fa9744dd6a140edc5f1
Meke <ladna_mekelive@yahoo.com>
2015-06-04 20:57:19 +0100

    first commit

---
+++ untitled.py
@@ -0,0 +1,31 @@
+import socket
+
+HOST, PORT = '', 8888
+
+listen_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+listen_socket.bind((HOST, PORT))
+
+listen_socket.listen(1)
+
+print 'Serving HTTP on port %s ...' %PORT
+
+while True:
+	client_connection, client_address = listen_socket.accept()
+	
+	print client_connection
+	print client_address
+
+	request = client_connection.recv(1024)
+	print request 
+
+	http_response = """\
+	HTTP/1.1 200 OK
+
+	Hello, World!
+	"""
+
+	client_connection.sendall(http_response)
+	client_connection.close()
